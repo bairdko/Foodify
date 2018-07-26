f5a541d369954dd43ab40b3c4a1db1ee3ad80756
Jeffry Valadez <jeffry.valadez.94@gmail.com>
2018-07-26 18:39:01 -0500

    creates markers on map

--- assets/project.js
+++ assets/project.js
@@ -1,15 +1,103 @@
-$(document).ready(function(){
+var map;
+var userPos = {lat: -34.397, lng: 150.644};
+var infowindow;
+var cuisine = "mexican";
+
+function initMap() {
+  pos = userPos;
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: pos,
+    zoom: 6
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: pos,
+          radius: 3000,
+          type: ['restaurant'],
+          keyword: [cuisine]
+        }, callback);
+      }
+
+  function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+     }
+    }
+  }
 
 
-  function location(){
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      //is this line of code redundant? could placeLoc variabe be used here?
+      position: place.geometry.location
+    });
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+                            'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+      infoWindow.open(map);
+    }
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  }
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
+      userPos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-      return pos;
+
+      infoWindow.setPosition(userPos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(userPos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
     });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
   }
+
+
+
+  // function location(){
+  //   navigator.geolocation.getCurrentPosition(function(position) {
+  //      pos = {
+  //       lat: position.coords.latitude,
+  //       lng: position.coords.longitude
+  //     };
+  //     // var cuisine = $("#cuisine").val();
+  //     // console.log("the value of search is " + cuisine);
+  //     // // console.log(userLocation);
+  //     // var apiKey = "AIzaSyBbk3RQCujaAyCVk8esnxZgSRP2RF_rRrg";
+  //     //
+  //     //
+  //     // var urlQuery = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + pos.lat + "," + pos.lng + "&radius=3000&type=restaurant&keyword=" + cuisine + "&key=" + apiKey;
+  //     //
+  //     // $.ajax({
+  //     //   url: urlQuery,
+  //     //   method: "GET"
+  //     // }).then(function(response){
+  //     //   console.log(response);
+  //     // });
+  //     return pos;
+  //   });
+  // }
+  //
+  // location();
 
 
 
@@ -19,24 +107,31 @@ $(document).ready(function(){
 
   $('#search').on("click",function(){
     event.preventDefault();
-    var userLocation = location();
+    // location();
 
 
-    var cuisine = $("#cuisine").val();
-    console.log("the value of search is " + cuisine);
-    // console.log(userLocation);
-    var apiKey = "AIzaSyBbk3RQCujaAyCVk8esnxZgSRP2RF_rRrg";
+    cuisine = $("#cuisine").val();
+    map.setCenter(userPos);
+    initMap();
 
-
-    var urlQuery = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + userLocation.lat + "," + userLocation.lng + "&radius=3000&type=restaurant&keyword=" + cuisine + "&key=" + apiKey;
-
-    $.ajax({
-      url: urlQuery,
-      method: "GET"
-    }).then(function(response){
-      console.log(response);
-    });
+    // console.log("the value of search is " + cuisine);
+    // // console.log(userLocation);
+    // var apiKey = "AIzaSyBbk3RQCujaAyCVk8esnxZgSRP2RF_rRrg";
+    //
+    //
+    // var urlQuery = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + pos.lat + "," + pos.lng + "&radius=3000&type=restaurant&keyword=" + cuisine + "&key=" + apiKey;
+    // console.log(pos.lat);
+    // console.log(pos.lng);
+    // $.ajax({
+    //   url: urlQuery,
+    //   method: "GET"
+    // }).then(function(response){
+    //   console.log(response);
+    // });
 
 
   });
-});
+
+
+
+//Document ready closing
